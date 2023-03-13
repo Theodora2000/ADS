c53d4900f0d644b0a5e5b2967b4eceab4126a6fb
Theodora2000 <xsimont@stuba.sk>
2023-03-13 20:22:20 +0100

    update: done

--- planmesta.py
+++ planmesta.py
@@ -1,27 +1,8 @@
-# import numpy as np
-
-# nabijacie_stanice = np.array(
-#     [
-#         [0, 0, 0, "S", 0],
-#         [0, 0, 0, 0, 0],
-#         [0, "S", 0, 0, 0],
-#         [0, 0, 0, 0, 0],
-#         ["S", 0, 0, 0, 0],
-#         [0, 0, "S", 0, "S"],
-#     ],
-#     dtype=object,
-# )
-
-# print(nabijacie_stanice)
+import math
 
 
-def printDistance(mat):
-    global n, m
-    ans = [[None] * m for i in range(n)]
-
-    for i in range(n):
-        for j in range(m):
-            ans[i][j] = 999999999999
+def calculateDistance(mat, m, n):
+    distance = [[math.inf] * m for i in range(n)]
 
     # i,j pozicia, na ktorej sme
     for i in range(n):
@@ -30,25 +11,50 @@ def printDistance(mat):
             for k in range(n):
                 for l in range(m):
                     if mat[k][l] == 1:
-                        ans[i][j] = min(ans[i][j], abs(i - k) + abs(j - l))
+                        distance[i][j] = min(distance[i][j], abs(i - k) + abs(j - l))
 
     for i in range(n):
         for j in range(m):
-            print(ans[i][j], end=" ")
+            print(distance[i][j], end=" ")
         print()
 
 
 if __name__ == "__main__":
-    n = 6
-    m = 5
-    mat = [
-        [0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [1, 0, 0, 0, 0],
-        [0, 0, 1, 0, 1],
+    n = 10
+    m = 4
+    charging_station = [
+        [0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 0, 0, 0],
+        [0, 0, 0, 1],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+    ]
+    # n = 17
+    # m = 4
+    charging_station2 = [
+        [0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 1],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [1, 0, 0, 0],
+        [0, 0, 0, 1],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
     ]
 
     # Function call
-    printDistance(mat)
+    calculateDistance(charging_station2, m, n)
